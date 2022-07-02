6e7092a78c21af579e3a82fdaf1bf5d3193b2e9b
JohnCYH <yhjchanaa@connect.ust.hk>
2022-07-02 16:18:22 +0800

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #EEEEEE;
+  display: flex;
+  flex-direction: column;
+  justify-contents: center;
+  align-items: center;
+`;
+const FormRow = styled.div`
+  width: 100%;
+  background-color: #DDDDDD;
+  display: flex;
+  justify-contents: space-between;
+`;
+
+const ActionButtonWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 30vh;
+  background-color: #CCCCCC;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ActionButtonLabel = styled.div`
+  font-size: 2em;
+  font-color: #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #555555;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormContainer>
+        <FormRow>
+          <ActionButtonWrapper>
+            <ActionButtonLabel>Yes</ActionButtonLabel>
+          </ActionButtonWrapper>
+          <ActionButtonWrapper>
+            <ActionButtonLabel>No</ActionButtonLabel>
+          </ActionButtonWrapper>
+        </FormRow>
+      </FormContainer>
     </div>
   );
 }
